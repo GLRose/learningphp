4446673a87ae40f8aef825ce34fd7990cd48677f
Garrett Rose <garrett.rose03@gmail.com>
2018-01-01 12:39:14 -0500

    New files added

---
+++ connect.php
@@ -0,0 +1,13 @@
+<?php
+  include_once 'includes/dbh.inc.php';
+ ?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    Hello this is some connection stuff
+  </body>
+</html>
