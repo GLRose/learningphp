9ee620f8856962dfd6e2abcf98f783fd395320a9
Garrett Rose <garrett.rose03@gmail.com>
2018-01-01 13:13:44 -0500

    added more database lesons

--- connect.php
+++ connect.php
@@ -8,6 +8,16 @@
     <title></title>
   </head>
   <body>
-    Hello this is some connection stuff
+    <?php
+      $sql = "SELECT * FROM users;";
+      $result = mysqli_query($conn, $sql);
+      $resultCheck = mysqli_num_rows($result);
+
+      if ($resultCheck > 0){
+        while ($row = mysqli_fetch_assoc($result)){
+          echo $row['user_first']. "<br>";
+        }
+      }
+     ?>
   </body>
 </html>
